8fdd22efa1260ddcea132f2332309d4fa07e54ca
Nicolay Poymanov <n.poymanov@gmail.com>
2018-07-21 10:10:13 +0300

    Add graph generation

--- main.py
+++ main.py
@@ -1,59 +1,22 @@
-import cv2
-import pandas
-from datetime import datetime
+from motion_detector import df
+from bokeh.plotting import figure
+from bokeh.io import output_file, show
+from bokeh.models import HoverTool, ColumnDataSource
 
-first_frame = None
-status_list = [None, None]
-times = []
-df = pandas.DataFrame(columns=['Start', 'End'])
+df['Start_string'] = df['Start'].dt.strftime('%Y-%m-%d %H:%M:%S')
+df['End_string'] = df['End'].dt.strftime('%Y-%m-%d %H:%M:%S')
 
-video = cv2.VideoCapture(0)
+cds = ColumnDataSource(df)
 
-while True:
-	check, frame = video.read()
-	status = 0
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	gray = cv2.GaussianBlur(gray, (21, 21), 0)
+p = figure(x_axis_type='datetime', height=100, title='Motion Graph',
+    sizing_mode='scale_width')
+p.yaxis.minor_tick_line_color = None
+p.ygrid[0].ticker.desired_num_ticks = 1
 
+hover = HoverTool(tooltips=[('Start:', '@Start_string'), ('End:', '@End_string')])
+p.add_tools(hover)
 
-	if first_frame is None:
-		first_frame = gray
-		continue
+q = p.quad(left='Start', right='End', bottom=0, top=1, color='green', source=cds)
 
-	delta_frame = cv2.absdiff(first_frame, gray)
-
-	thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
-	thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
-
-	(_,cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-	for contour in cnts:
-		if cv2.contourArea(contour) < 10000:
-			continue
-		status = 1
-		(x, y, w, h) = cv2.boundingRect(contour)
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
-
-	status_list.append(status)
-
-	if status_list[-1] == 1 and status_list[-2] == 0:
-		times.append(datetime.now())
-	if status_list[-1] == 0 and status_list[-2] == 1:
-		times.append(datetime.now())
-
-	cv2.imshow('Capturing', frame)
-
-	key = cv2.waitKey(1)
-
-	if key == ord('q'):
-		if status == 1:
-			times.append(datetime.now())
-		break
-
-for i in range(0, len(times), 2):
-	df = df.append({'Start': times[i], 'End': times[i+1]}, ignore_index=True)
-
-df.to_csv('times.csv')
-
-video.release()
-cv2.destroyAllWindows()
+output_file('graph.html')
+show(p)
